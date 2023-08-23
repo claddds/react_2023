b28b430b6ff642c3ac0ecaff26cc413c5c20a1ce
ryuhyeji <hyeji5207@gmail.com>
2023-08-23 12:57:59 +0900

    0823-2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-
+import React from 'react';
 import Day from './component/Day';
 import DayList from './component/DayList';
 import Header from './component/Header';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 
+// 1. App를 BrowserRouter로 감싼다.
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <DayList />
-      <Day />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+        {/**첫 페이지를 의미한다. */}
+        <Route path="/">
+          <DayList />
+        </Route> 
+
+        <Route path="/day">
+          <Day />
+        </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   ); 
 }
 export default App;
