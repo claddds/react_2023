33fad5ce3bb39170a682b6c784f6fecb80d223ba
ryuhyeji <hyeji5207@gmail.com>
2023-08-24 11:37:18 +0900

    0824-main01

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,11 +1,14 @@
 import dummy from "../db/data.json";
+import {Link} from "react-router-dom";
+
 export default function DayList(){
     // console.log(dummy);
     // json같은 반복처리할때는 주로 map을 사용
+    // react
     return (
         <ul className="list_day">
             {dummy.days.map(k => (
-                <li key={k.id}>Day {k.day}</li>
+                <li key={k.id}><Link to ={`/day/${k.day}`}>Day{k.day}</Link></li>
             ))}    
         </ul>
     );
